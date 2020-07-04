45f18bb69ff954ab440a9a2e073ed467b723f792
karunsane <karunsane@gmail.com>
2020-07-04 16:30:46 -0500

    Control de errores con @ControllerAdvice y plantillas personalizadas y genéricas

--- src/main/java/com/learning/springboot/error/app/controllers/AppController.java
+++ src/main/java/com/learning/springboot/error/app/controllers/AppController.java
@@ -6,10 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AppController {
 	
+	@SuppressWarnings("unused")
 	@GetMapping("/index")
 	private String index() {
-		@SuppressWarnings("unused")
-		Integer valor = 100/0;
+		//
+		//Arithmetic Exception
+		//Integer valor = 100/0;
+		//
+		//NumberFormatException
+		Integer valor = Integer.parseInt("10x");
 		return "index";
 	}
 
